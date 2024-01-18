82375d422844af4bcd2dc89db89ab3e71002a9c8
Pedro Ascenso <114543288+pedroascenso@users.noreply.github.com>
2024-01-18 17:24:54 +0000

    Update Demo.java

--- src/main/java/com/microsoft/demo/Demo.java
+++ src/main/java/com/microsoft/demo/Demo.java
@@ -1,7 +1,13 @@
 package com.microsoft.demo;
 
 public class Demo {
-    public void DoSomething(boolean flag){
+
+    public static main void(String[] args) {
+
+        Demo.DoSomething(true);
+    }
+    
+    public static void DoSomething(boolean flag){
         if(flag){
             System.out.println("I am covered");
             return;
